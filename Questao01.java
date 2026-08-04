b945b438bf93f71fca723269d17cd6f7d6312569
Mateus <mateus_andrade18@hotmail.com>
2026-08-03 21:37:46 -0300

    primeiro commit

---
+++ Questao01.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Questao01 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+
+            System.out.print("Primeiro número: ");
+
+            int a = sc.nextInt();
+
+            System.out.print("Segundo número: ");
+
+            int b = sc.nextInt();
+
+            System.out.println("Soma: " + (a + b));
+
+            System.out.println("Subtração: " + (a - b));
+
+            System.out.println("Multiplicação: " + (a * b));
+            
+}
+    
