06db0ea7708b09dce1cfefdd1a8f7786b4922591
Mateus <mateus_andrade18@hotmail.com>
2026-08-03 21:44:49 -0300

    quarto

--- Questao01.java
+++ Questao01.java
@@ -2,29 +2,21 @@ import java.util.Scanner;
 
 public class Questao01 {
 
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        try {
-aaaaaaaaaaaaaaaaa
-            System.out.print("Primeiro número: ");
-
-            int a = sc.nextInt();
-
-            System.out.print("Segundo número: ");
-
-            int b = sc.nextInt();
-
-            System.out.println("Soma: " + (a + b));
-
-            System.out.println("Subtração: " + (a - b));
-
-            System.out.println("Multiplicação: " + (a * b));
-        } catch (java.util.InputMismatchException e) {
-            System.out.println("Entrada inválida.");
-        } finally {
-            sc.close();
-        }
-    }
+   public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       try {
+           System.out.print("Primeiro número: ");
+           int a = sc.nextInt();
+           System.out.print("Segundo número: ");
+           int b = sc.nextInt();
+           System.out.println("Soma: " + (a + b));
+           System.out.println("Subtração: " + (a - b));
+           System.out.println("Multiplicação: " + (a * b));
+           System.out.println("Divisão: " + (a / b));
+           System.out.println("Resto: " + (a % b));
+       } catch (Exception e) {
+           System.out.println("Valor inválido!");
+       }
+       sc.close();
+   }
 }
