051624dee8d26b7eadd888c3e7ae4ab5fd99ce91
Mateus <mateus_andrade18@hotmail.com>
2026-08-03 21:59:19 -0300

    criei a questao 2

--- Questao01.java
+++ Questao01.java
@@ -6,15 +6,25 @@ public class Questao01 {
        Scanner sc = new Scanner(System.in);
        try {
            System.out.print("Primeiro número: ");
+
            int a = sc.nextInt();
+
            System.out.print("Segundo número: ");
+
            int b = sc.nextInt();
+
            System.out.println("Soma: " + (a + b));
+
            System.out.println("Subtração: " + (a - b));
+
            System.out.println("Multiplicação: " + (a * b));
+
            System.out.println("Divisão: " + (a / b));
+
            System.out.println("Resto: " + (a % b));
+
        } catch (Exception e) {
+        
            System.out.println("Valor inválido!");
        }
        sc.close();
---
+++ Questao02.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Questao02 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+
+            System.out.print("Digite sua idade: ");
+
+            int idade = sc.nextInt();
+
+            if (idade >= 60) {
+
+                System.out.println("Idoso");
+
+            } else if (idade >= 18) {
+
+                System.out.println("Maior de idade");
+
+            } else {
+
+                System.out.println("Menor de idade");
+
+            }
+
+        } catch (Exception e) {
+
+            System.out.println("Idade inválida!");
+
+        }
+
+        sc.close();
+
+    }
+
+}
